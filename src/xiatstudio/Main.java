80e9e6bc2df9de2efd8f306981105435c5b3b106
t.seb.xia <41295316+OMGCA@users.noreply.github.com>
2018-11-12 11:27:47 +0000

    Updated UI for mode selection

--- src/xiatstudio/Main.java
+++ src/xiatstudio/Main.java
@@ -14,6 +14,7 @@ import java.io.FileWriter;
 import java.io.FilenameFilter;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
+import java.util.Arrays;
 import java.util.Date;
 
 import javax.imageio.ImageIO;
@@ -31,10 +32,12 @@ public class Main extends JFrame {
 	/* Default loading data */
 	static String data = "./Benson_Data/empty.txt";
 	static JPanel panel;
+	static int displayMode = 0;
 
 	public static void main(String[] args) {
 		/* Load GUI component */
 		GUISetup();
+
 	}
 
 	public static void GUISetup() {
@@ -48,15 +51,30 @@ public class Main extends JFrame {
 		/* Initialize JFrame and Menu bar */
 		JFrame frame = new JFrame();
 		JMenuBar menuBar = new JMenuBar();
-		JMenu menu, exportMenu, exportAllMenu;
+		JMenu menu, menu2, menu3, exportMenu, exportAllMenu;
 		JMenuItem menuItem, menuItem2, menuItem3, menuItem4, menuItem5;
+		JMenuItem mode1, mode2, mode3, mode4;
+		JMenuItem pen_offON, pen_offOFF;
 
 		/* Background color */
 		Color bg = new Color(54, 63, 70);
 
 		menu = new JMenu("File");
+		menu2 = new JMenu("Mode");
+		menu3 = new JMenu("Pen off tracking");
+		pen_offON = new JMenuItem("ON");
+		pen_offOFF = new JMenuItem("OFF");
+		menu3.add(pen_offON);
+		menu3.add(pen_offOFF);
+
 		menuBar.add(menu);
+		menuBar.add(menu2);
+		menuBar.add(menu3);
 		menuItem = new JMenuItem("Open");
+		mode1 = new JMenuItem("All");
+		mode2 = new JMenuItem("Horizontal");
+		mode3 = new JMenuItem("Vertical");
+		mode4 = new JMenuItem("Oblique");
 		exportMenu = new JMenu("Export as...");
 		exportAllMenu = new JMenu("Export all as...");
 		menuItem2 = new JMenuItem("PNG Image");
@@ -66,6 +84,10 @@ public class Main extends JFrame {
 		menu.add(menuItem);
 		menu.add(exportMenu);
 		menu.add(exportAllMenu);
+		menu2.add(mode1);
+		menu2.add(mode2);
+		menu2.add(mode3);
+		menu2.add(mode4);
 		exportMenu.add(menuItem2);
 		exportMenu.add(menuItem3);
 
@@ -81,6 +103,60 @@ public class Main extends JFrame {
 		frame.setSize(1280, 720);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setVisible(true);
+
+		mode1.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				displayMode = 0;
+				panel.repaint();
+
+			}
+		});
+
+		mode2.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				displayMode = 1;
+				panel.repaint();
+
+			}
+		});
+
+		mode3.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				displayMode = 2;
+				panel.repaint();
+
+			}
+		});
+
+		mode4.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				displayMode = 3;
+				panel.repaint();
+
+			}
+		});
+
+		pen_offON.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				displayMode = 4;
+				panel.repaint();
+
+			}
+		});
+
+		pen_offOFF.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				displayMode = 0;
+				panel.repaint();
+
+			}
+		});
 
 		/* Open file action */
 		menuItem.addActionListener(new ActionListener() {
@@ -315,7 +391,7 @@ public class Main extends JFrame {
 
 			g2.setRenderingHints(hints);
 			Benson testFigure = new Benson(data);
-			testFigure.drawBenson(g2);
+			testFigure.drawBenson(g2, displayMode);
 		}
 	}
 
